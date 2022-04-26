ad53de098597cc7c34954c8cb8b296f6c299537d
mencisom@unbosque.edu.co <100002319+Mencisom@users.noreply.github.com>
2022-04-25 22:36:55 -0500

    logic2.0

--- src/main/java/co/edu/unbosque/bluemarket3_1/RestApplication.java
+++ src/main/java/co/edu/unbosque/bluemarket3_1/RestApplication.java
@@ -4,5 +4,4 @@ import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
 @ApplicationPath("/api")
-public class RestApplication extends Application {
-}
+public class RestApplication extends Application {}
